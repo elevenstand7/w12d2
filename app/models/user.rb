9e902d3cf672b5d7b384c56963ea15b0e5da7b89
elevenstand7 <elevenstand7@gmail.com>
2023-09-12 15:40:15 -0400

    fixed some bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,15 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :username, :email, :session_token, presence: true, uniqueness: true
-  validates :username, length: { in: 3..30 }, format: { without: URI::MailTo::EMAIL_REGEXP, message: "can't be an email"},
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, length:{in: 3..255},
+  validates :username, length: { in: 3..30 }, format: { without: URI::MailTo::EMAIL_REGEXP, message: "can't be an email"}
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, length:{in: 3..255}
   validates :password, length:{in: 6..255}, allow_nil:true
+
+  before_validation :ensure_session_token
 
   def generate_unique_session_token
     while true
-      let token = SecureRandom.base64
+        token = SecureRandom.base64
       return token unless User.exists?(session_token: token)
     end
   end
